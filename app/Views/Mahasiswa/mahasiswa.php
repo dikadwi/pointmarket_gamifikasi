259749a55af83b2033e5827464edef0646ddae90
dikadwi <adwiarko@gmail.com>
2024-11-27 20:20:50 +0700

    update

--- app/Views/Mahasiswa/mahasiswa.php
+++ app/Views/Mahasiswa/mahasiswa.php
@@ -8,7 +8,7 @@
         <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modalTambahMahasiswa">Input</button>
         <h3>
             <!-- <center>Data Mahasiswa<center> -->
-            <center>Data User Data baru(Mahasiswa)<center>
+            <center>Data User (Data Mahasiswa)<center>
         </h3>
     </div>
 
